d159a33589f35a5c4bd83107f412e27340c4bf76
chris-townsend <chris-townsend@live.co.uk>
2023-02-08 11:33:33 +0000

    Add Profile model

---
+++ profiles/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=255, blank=True)
+    content = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/',
+        default='../default_profile_y0pdym'
+    )
+
+class Meta:
+    ordering = ['-created_date']
+
+    def __str__(self):
+        return f"{self.owner}'s profile"
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
+
+post_save.connect(create_profile, sender=User)
