1ac1299e08f1afd88a1949a3502f05da27f8c4d8
chris-townsend <chris-townsend@live.co.uk>
2023-02-08 12:06:41 +0000

    Add profile serializer

--- profiles/models.py
+++ profiles/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
+
 class Profile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
@@ -13,14 +14,17 @@ class Profile(models.Model):
         default='../default_profile_y0pdym'
     )
 
+
 class Meta:
     ordering = ['-created_date']
 
     def __str__(self):
         return f"{self.owner}'s profile"
 
+
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(owner=instance)
+
 
 post_save.connect(create_profile, sender=User)
