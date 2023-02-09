cae226df50ce6f735b2526757ca1da35dc1e6aa7
chris-townsend <chris-townsend@live.co.uk>
2023-02-09 09:45:42 +0000

    Add login/out pages with IsOwner permissiions

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -4,10 +4,14 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         model = Profile
         fields = [
-            'id', 'owner', 'created_date', 'updated_date', 'name', 'content', 'image'
-        ]
-        
+            'id', 'owner', 'created_date', 'updated_date', 'name',
+            'content', 'is_owner']
