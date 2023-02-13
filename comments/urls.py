47158db3b5575c36dd9c1c9b9b4db4db68f6fc94
chris-townsend <chris-townsend@live.co.uk>
2023-02-13 11:40:32 +0000

    Add likes app with LikeSerializer and view

--- comments/urls.py
+++ comments/urls.py
@@ -5,5 +5,4 @@ from comments import views
 urlpatterns = [
     path('comments/', views.CommentList.as_view()),
     path('comments/<int:pk>', views.CommentDetail.as_view()),
-
 ]
--- comments/views.py
+++ comments/views.py
@@ -17,4 +17,3 @@ class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = CommentDetailSerializer
     queryset = Comment.objects.all()
-    
