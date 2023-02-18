c31cf76c32f74ca9872f18060e81fd194cf7099b
chris-townsend <chris-townsend@live.co.uk>
2023-02-18 08:48:49 +0000

    Update urls.py

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -20,7 +20,7 @@ from .views import root_route, logout_route
 urlpatterns = [
     path('', root_route),
     path('admin/', admin.site.urls),
-    path('api-auth', include('rest_framework.urls')),
+    path('api-auth/', include('rest_framework.urls')),
     path('dj-rest-auth/logout/', logout_route),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
